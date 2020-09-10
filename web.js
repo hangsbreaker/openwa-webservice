c06889889931a5e4b9308e80590d92f0000ca522
Hangs Breaker <hangsbreaker@gmail.com>
2020-09-10 11:45:13 +0700

    Update web.js

--- web.js
+++ web.js
@@ -18,10 +18,10 @@ http
       });
       req.on("end", () => {
         let post = parse(body);
-        console.log(post);
+        //console.log(post);
         //res.end(JSON.stringify(post));
-        sendMessage(post.to, post.msg);
-        res.end(webpage(JSON.stringify(post)));
+        sendMessage(post.to, post.msg, post.jenis, post.filename, post.media);
+        res.end(webpage(post.to + ": " + post.msg));
       });
     } else {
       res.end(webpage());
@@ -29,8 +29,14 @@ http
   })
   .listen(8080);
 
-function sendMessage(to, msg) {
-  theclient.sendText(to, msg);
+function sendMessage(to, msg, jenis = "", filename = "", media = "") {
+  if (jenis === "") {
+    theclient.sendText("" + to, msg);
+  } else if (jenis === "image") {
+    theclient.sendImage(to, media, filename, msg);
+  } else if (jenis === "file") {
+    theclient.sendFile(to, media, filename, msg);
+  }
 }
 
 function webpage(param = "") {
@@ -40,10 +46,54 @@ function webpage(param = "") {
     <body>
       ${param}
       <form action="/" method="post">
-        <input type="text" name="to" placeholder="To" /><br />
-        <input type="text" name="msg" placeholder="Message" /><br />
-        <button>Save</button>
+        <input type="text" name="to" placeholder="To" style="display:block;"/>
+        <input type="text" id="msg" name="msg" placeholder="Message" style="display:block;"/>
+        <select name="jenis" onchange="showhide(this);">
+          <option value=""> No Media </option>
+          <option value="image">Image</option>
+          <option value="file">File</option>
+        </select><br>
+        <div id="file" style="display:none;">
+          <input type="file" id="data" onchange="tobase64()"/>
+          <input type="hidden" id="media" name="media"/>
+          <input type="hidden" id="filename" name="filename"/>
+        </div>
+        <button>Send</button>
       </form>
+      <script>
+      function getBase64(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => resolve(reader.result);
+          reader.onerror = error => reject(error);
+        });
+      }
+
+      function showhide(me){
+        if(me.value != ""){
+          document.getElementById("file").style.display="block";
+        }else{
+          document.getElementById("file").style.display="none";
+        }
+        if(me.value == "file"){
+          document.getElementById("msg").style.display="none";
+        }else{
+          document.getElementById("msg").style.display="block";
+        }
+      }
+      
+      function tobase64(){
+        var file = document.getElementById('data').files[0];
+        getBase64(file).then(
+          data => {
+            //console.log(data);
+            document.getElementById('media').value=data;
+            document.getElementById('filename').value=file.name;
+          }
+        );
+      }
+      </script>
     </body>
     </html>
   `;
